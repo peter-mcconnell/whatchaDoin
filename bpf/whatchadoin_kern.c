9665b7b1bd2425ade07e66791af674969b2c4b90
Peter McConnell <me@petermcconnell.com>
2023-05-19 09:30:05 +0000

    more boilerplate

--- bpf/whatchadoin_kern.c
+++ bpf/whatchadoin_kern.c
@@ -1,9 +1,30 @@
 #include <linux/bpf.h>
 #include <bpf_helpers.h>
 
-int whatchadoin()
+#define TASK_COMM_LEN 16
+
+// this structure copied from "cat /sys/kernel/tracing/events/syscalls/sys_enter_execve/format"
+struct syscalls_enter_execve {
+    unsigned short common_type;
+    unsigned char common_flags;
+    unsigned char common_preempt_count;
+    int common_pid;
+
+    long int __syscall_nr;
+    const char *filename;
+    const char *const *argv;
+    const char *const *envp;
+};
+
+SEC("tp/syscalls/sys_enter_execve")
+int whatchadoin(struct syscalls_enter_execve *ctx)
 {
-	return 0;
+	char fmt[] = "%s (pid: %d)\n";
+  char comm[TASK_COMM_LEN];
+  bpf_get_current_comm(&comm, sizeof(comm));
+  bpf_trace_printk(fmt, sizeof(fmt), comm, bpf_get_current_pid_tgid() >> 32);
+
+  return 0;
 }
 
 char _license[] SEC("license") = "GPL";
--- main.go
+++ main.go
@@ -2,8 +2,60 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cilium/ebpf"
 )
 
+const (
+	bpfObjectName = "whatchadoin_kern"
+	mapName       = "process_map"
+)
+
+type ProcessInfo struct {
+	PID  uint32
+	Name [16]byte
+}
+
 func main() {
+	spec, err := ebpf.LoadCollectionSpec(fmt.Sprintf("bpf/%s.o", bpfObjectName))
+	if err != nil {
+		panic(err)
+	}
+
+	coll, err := ebpf.NewCollection(spec)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create new collection: %v\n", err))
+	}
+	defer coll.Close()
+
+	prog := coll.Programs["whatchadoin"]
+	if prog == nil {
+		panic("No program named 'whatchadoin' found in collection")
+	}
+
+	// Create a signal handler to capture Ctrl+C and exit gracefully
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
 	fmt.Println("Starting ...")
+
+	go func() {
+		for {
+			select {
+			case <-signals:
+				return
+			}
+		}
+	}()
+
+	// Wait until the user interrupts the program
+	<-signals
+}
+
+func getProcessConnections(pid int) ([]string, error) {
+	connections := []string{}
+	return connections, nil
 }
